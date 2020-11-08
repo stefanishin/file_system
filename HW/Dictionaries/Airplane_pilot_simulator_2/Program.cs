f8252a5614d6f24c36f4663c8683ba1546ec09bc
Stefanishin Mikita <stefanishin92@gmail.com>
2020-11-08 12:27:52 +0200

    add all tasks 5. lab-3,hw-2

---
+++ HW/Dictionaries/Airplane_pilot_simulator_2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Airplane_pilot_simulator_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
